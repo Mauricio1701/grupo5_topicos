4418258e5cfd621606d08a34b6aa93e8ed4c3bf3
TheDrianN <edelgadovidarte@gmail.com>
2025-06-14 14:32:14 -0500

    update seeder

--- database/seeders/GroupDetailSeeder.php
+++ database/seeders/GroupDetailSeeder.php
@@ -4,7 +4,7 @@ namespace Database\Seeders;
 
 use Illuminate\Database\Console\Seeds\WithoutModelEvents;
 use Illuminate\Database\Seeder;
-use App\Models\GroupDetail;
+use App\Models\Groupdetail;
 
 class GroupDetailSeeder extends Seeder
 {
@@ -13,32 +13,32 @@ class GroupDetailSeeder extends Seeder
      */
     public function run(): void
     {
-        $groupDetail = new GroupDetail();
+        $groupDetail = new Groupdetail();
         $groupDetail->scheduling_id = 1;
         $groupDetail->employee_id = 1;
         $groupDetail->save();
 
-        $groupDetail = new GroupDetail();
+        $groupDetail = new Groupdetail();
         $groupDetail->scheduling_id = 1;
         $groupDetail->employee_id = 2;
         $groupDetail->save();
 
-        $groupDetail = new GroupDetail();
+        $groupDetail = new Groupdetail();
         $groupDetail->scheduling_id = 1;
         $groupDetail->employee_id = 3;
         $groupDetail->save();
 
-        $groupDetail = new GroupDetail();
+        $groupDetail = new Groupdetail();
         $groupDetail->scheduling_id = 2;
         $groupDetail->employee_id = 4;
         $groupDetail->save();
 
-        $groupDetail = new GroupDetail();
+        $groupDetail = new Groupdetail();
         $groupDetail->scheduling_id = 2;
         $groupDetail->employee_id = 5;
         $groupDetail->save();
 
-        $groupDetail = new GroupDetail();
+        $groupDetail = new Groupdetail();
         $groupDetail->scheduling_id = 2;
         $groupDetail->employee_id = 6;
         $groupDetail->save();
